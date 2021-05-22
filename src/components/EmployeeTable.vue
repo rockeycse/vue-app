8e06865c138f900293d26c82fbe4cd631351194c
Rockey Ahmed <rockeycse@gmail.com>
2021-05-22 21:57:39 +0600

    edit employee

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -16,15 +16,15 @@
           </td>
           <td v-else>{{ employee.name }}</td>
           <td v-if="editing === employee.id">
-            <input type="text" v-model="employee.name" />
+            <input type="text" v-model="employee.email" />
           </td>
           <td v-else>{{ employee.email }}</td>
           <td v-if="editing === employee.id">
             <button @click="editEmployee(employee)">Save</button>
-            <button class="muted-button" @click="editing = null">Cancel</button>
+            <button class="muted-button" @click="cancelEdit(employee)">Cancel</button>
           </td>
           <td v-else>
-            <button @click="editMode(employee.id)">Edit</button>
+            <button @click="editMode(employee)">Edit</button>
             <button @click="$emit('delete:employee', employee.id)">
               Delete
             </button>
@@ -47,8 +47,14 @@ export default {
     };
   },
   methods: {
-    editMode(id) {
-      this.editing = id;
+    editMode(employee) {
+      this.cachedEmployee = Object.assign({}, employee);
+      this.editing = employee.id;
+    },
+
+    cancelEdit(employee) {
+      Object.assign(employee, this.cachedEmployee);
+      this.editing = null;
     },
 
     editEmployee(employee) {
